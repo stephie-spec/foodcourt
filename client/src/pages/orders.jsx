6d26f6318f95782a4cbc1f62ccffe218ba509801
stephie-spec <compwork254@gmail.com>
2026-02-13 08:36:07 +0300

    updates order.jsx

--- client/src/pages/orders.jsx
+++ client/src/pages/orders.jsx
@@ -7,12 +7,13 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Navbar from '@/components/navbar';
 import { createOrder } from '@/lib/apiHelper';
-import { getCustomerOrders } from '@/lib/apiHelper';
+import { apiHelper } from '@/lib/apiHelper';
 
 
 const API_BASE = 'http://localhost:5555';
 
 export default function OrdersPage() {
+  const { getCustomerOrders } = apiHelper;
   const router = useRouter();
   const { data: session, status } = useSession();
   const [menuData, setMenuData] = useState([]);
